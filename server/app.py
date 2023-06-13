909e28d4376e81e9e67e766cb9b78bb682e2ab45
Historia Montague <historia.montague@outlook.com>
2023-06-13 10:14:17 -0600

    Finished Lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,36 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    return f"""
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+            """
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    animals = [f"<ul>Animal: {animal.name}</ul>" for animal in zookeeper.animals]
+    return f"""
+            <ul>ID: {zookeeper.id}</ul>
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+            {"".join(animals)}
+            """
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    animals = [f"<ul>Animal: {animal.name}</ul>" for animal in enclosure.animals]
+    return f"""
+            <ul>ID: {enclosure.id}</ul>
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+            {"".join(animals)}
+            """
 
 
 if __name__ == '__main__':
